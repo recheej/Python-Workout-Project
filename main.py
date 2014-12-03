26bbc2c63a37ccced3d0992fa0644a36e9ff25a3
= <recheejozil@hotmail.com>
2014-12-03 02:25:12 -0500

    test change commit

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import Login
 if __name__ == '__main__':
     app = QtGui.QApplication([])
     window = Login.LoginWindow()
-    
+
+    #this is a test comment
+    print 2
+
+    #more testing
     window.show()
     app.exec_()
